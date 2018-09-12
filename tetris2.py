8f4e8acd04364c7c5019f141c20282b9922af7c3
DESKTOP-7B4UMH3\Jarred Moyer <jarredds941@gmail.com>
2018-09-11 21:07:06 -0400

    Finished project implementaton and added basic comments

--- tetris2.py
+++ tetris2.py
@@ -159,6 +159,7 @@ def backward_el():
     turtle.forward(20)
     turtle.left(90)
     turtle.backward(10)
+    turtle.down()
 
 def reset_stack():
     '''
@@ -170,8 +171,10 @@ def reset_stack():
     turtle.left(90)
     turtle.down()
 def return_home(x,y):
+    '''
+    Moves the cursor left x and down y; used to return the cursor to the home position
+    '''
     turtle.up()
-    revert_rotate()
     turtle.right(90)
     turtle.forward(y*10)
     turtle.right(90)
@@ -181,6 +184,9 @@ def return_home(x,y):
     turtle.down()
 
 def move(x,y):
+    '''
+    Moves the cursor right x and up y
+    '''
     turtle.up()
     turtle.forward(x*10)
     turtle.left(90)
@@ -191,14 +197,89 @@ def move(x,y):
 
     
 def rotate_shape(angle):
+    '''
+    Rotates the shape that is about to be constructed by a given angle, returns the angle such that it can be accounted for globally
+    '''
     turtle.left(angle)
     return angle
 
 def revert_rotate(angle):
+    '''
+    Reverts the rotation of rotate_shape
+    '''
     turtle.right(angle)
 
+def shift_down():
+    '''
+    shifts the WIP block down one
+    '''
+    turtle.up()
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
+    turtle.down()
+def draw_s():
+    '''
+    Draws the s shape
+    '''
+    turtle.fillcolor('purple')
+    draw_block()
+    shift_right()
+    draw_block()
+    shift_up()
+    draw_block()
+    shift_right()
+    draw_block()
+    turtle.up()
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
+    turtle.backward(20)
+    turtle.down()
+
+def draw_z():
+    '''
+    Draws the z shape
+    '''
+    turtle.fillcolor('orange')
+    draw_block()
+    shift_right()
+    draw_block()
+    turtle.up()
+    turtle.backward(10)
+    turtle.down()
+    shift_up()
+    draw_block()
+    shift_left()
+    draw_block()
+    turtle.up()
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.left(90)
+
+def draw_t():
+    '''
+    Draws the t shape
+    '''
+    turtle.fillcolor('grey')
+    draw_block()
+    shift_right()
+    draw_block()
+    shift_up()
+    draw_block()
+    shift_down()
+    shift_right()
+    draw_block()
+    turtle.up()
+    turtle.backward(20)
+    turtle.down()
+    
 def interpret_input(input_shape):
-    if(input_shape == 's' or input_shape == 'S'):
+    '''
+    Draws a shape given a letter input
+    '''
+    if(input_shape == 'b' or input_shape == 'B'):
         draw_square()
     elif(input_shape == 'i' or input_shape == 'I'):
         draw_line()
@@ -206,16 +287,47 @@ def interpret_input(input_shape):
         draw_el()
     elif(input_shape == 'j' or input_shape =='J'):
         backward_el()
+    elif(input_shape == 's' or input_shape == 'S'):
+        draw_s()
+    elif(input_shape == 'z' or input_shape =='Z'):
+        draw_z()
+    elif(input_shape == 't' or input_shape == 'T'):
+        draw_t()
     
 
+    
 turtle.speed(0)
 draw_board()
-inp = input('What shape')
-TURTLE_OFFSET=rotate_shape(90)
-test = input('go?')
-print(TURTLE_OFFSET)
+interpret_input('b')
+move(2,0)
+interpret_input('i')
+return_home(2,0)
+move(3,0)
+interpret_input('s')
+return_home(3,0)
+move(5,0)
+interpret_input('t')
+return_home(5,0)
+move(8,0)
+interpret_input('z')
+return_home(8,0)
+move(4,2)
+interpret_input('l')
+return_home(4,2)
+move(7,2)
+interpret_input('j')
+return_home(7,2)
+shape_input = input('Enter a letter {BILJZST} to choose the shape to place: ')
+angle_input = input('Enter 0,90,180,or 270 for the rotation: ')
+row_input = input('Enter row number (0 to 19) from the bottom left space: ')
+col_input = input('Enter column number (0 to 19) from the bottom left space: ')
+
+move(int(row_input),int(col_input))
+TURTLE_OFFSET=rotate_shape(int(angle_input))
+interpret_input(shape_input)
 revert_rotate(TURTLE_OFFSET)
-interpret_input(inp)
+return_home(int(row_input),int(col_input))
+
 turtle.done()
 
 

fb652e308f65cfa2636abce4297c43da4ed4df84
DESKTOP-7B4UMH3\Jarred Moyer <jarredds941@gmail.com>
2018-09-27 18:47:19 -0400

    Implemented turning and square

--- shapy_turtle.py
+++ shapy_turtle.py
@@ -3,24 +3,35 @@ import turtle
 
 def get_num(ch):
     temp_list = ""
-    if(!(ch[0].isdigit())):
+    if((ch[0].isdigit() == False)):
         return None
     for i in ch:
         if(i.isdigit()):
             temp_list = temp_list + i
+            
 
         else:
-            return
-
+            return temp_list
+    return temp_list
 def proccess_left(string):
-    pass
+
+    x = get_num(string)
+    turtle.left(int(x))
+    return string[len(str(x)):]
 
 
 def proccess_right(string):
-    pass
+    
+    x = get_num(string)
+    turtle.left(int(x))
+    return string[len(str(x)):]
 
 def proccess_square(string):
-    pass
+    x = get_num(string)
+    for i in range(0,4):
+        turtle.left(90)
+        turtle.forward(int(x))
+    return string[len(str(x)):]
 
 def process_triangle(string):
     pass
@@ -30,7 +41,7 @@ def proccess_circle(string):
 
 def proccess_forward(string):
     pass
-def proccess_backward(string)
+def proccess_backward(string):
     pass
 
 def proccess_up(string):
@@ -104,19 +115,4 @@ def process_commands(string):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+process_commands('S90')

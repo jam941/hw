e99253b41e86a19f9a180ea7492db94bb7228dae
DESKTOP-7B4UMH3\Jarred Moyer <jarredds941@gmail.com>
2018-10-18 18:44:31 -0400

    Work on weeks 6-8

---
+++ cross.py
@@ -0,0 +1,26 @@
+import turtle
+
+def cross1(length):
+
+    turtle.forward(length)
+    turtle.up()
+    turtle.backward(length)
+    turtle.down()
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.up()
+    turtle.backward(length)
+    turtle.down()
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.up()
+    turtle.backward(length)
+    turtle.down()
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.up()
+    turtle.backward(length)
+    turtle.down()
+    turtle.left(90)
+
+cross1(100)

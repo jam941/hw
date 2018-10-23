b59db6baa88747b096d26033f5f66474593f1151
DESKTOP-7B4UMH3\Jarred Moyer <jarredds941@gmail.com>
2018-10-23 18:47:39 -0400

    Further work on first greedy function

--- lab7/moving.py
+++ lab7/moving.py
@@ -26,3 +26,28 @@ def import_data(path):
         item_list.append(it)
     return box_list,item_list
 
+def sort_data(lst):
+    return sorted(lst, 'weight',)
+def strat1(boxes,items):
+    sorted_items = sort_data(items)
+
+    for i in sorted_items:
+
+        test_cap = 0
+        best = 0
+        count = 0
+        for k in boxes:
+            cap = k.remaing_cap()
+
+            if(cap>test_cap):
+                best = count
+                test_cap = cap
+            count += 1
+        boxes[best].items.append(i)
+        boxes[best].remaining_cap = boxes[best].remaining_cap - i.weight
+
+
+data = import_data('data.txt')
+print(data)
+print('Line break to show next is sorted         S')
+print(sort_data(data))

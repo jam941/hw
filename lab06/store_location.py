763f6f88c43eaa62a6d12de67c40d33c40573bd0
Jarred Moyer <jarredds941@gmail.com>
2018-10-23 20:48:06 -0400

    Final code for lab6

--- lab06/store_location.py
+++ lab06/store_location.py
@@ -1,15 +1,11 @@
-import math
+import time
 def import_data(path):
     f = open(path)
     lst = []
     for i in f:
         count = 0
-        print(i)
-        for k in i:
-            if(k.isdigit()):
-                break
-            count += 1
-        lst.append((i[count:-1]))
+        num = i.split()
+        lst.append(int(num[1]))
 
     return lst
 
@@ -18,7 +14,7 @@ def quick_sort(data):
     if data == []:
         return data
     else:
-        print(data)
+
         pivot = data[0]
 
         less  = []
@@ -32,34 +28,42 @@ def quick_sort(data):
                 less.append(i)
             else:
                 same.append(i)
+
         return quick_sort(less) + same + quick_sort(more)
 
-def find_med(lst):
-    sort = quick_sort(lst)
+def find_med(data):
+
+    lst = quick_sort(data)
+
     length  = int(len(lst))
     if(length%2 == 0):
-        return int(lst[length/2]+lst[length/2 -1])/2
+        end = time.time()
+        print(end)
+        return int(lst[int(length/2)]+lst[int(length/2) -1])/2
     else:
+        end = time.time()
         return int(lst[int(length/2)])
 
 def distance(lst, shop):
     sum = 0
     for i in lst:
-        #sum += abs(i-shop)
-        pass
+        dis = abs(shop-i)
+        sum += dis
     return sum
 
 
 def main():
-    #path = input('What file would you like to open?  ')
-    path = 'data'
+    path = input('What file would you like to open?: ')
+
     data = import_data(path)
-
+    start = time.time()
     shop = find_med(data)
-
+    end = time.time()
+    elapsed = end-start
     total_distance = distance(data,shop)
     print('The optimal shop placement is at ',shop)
     print('The total distances to offices is ',total_distance)
+    print('Elapsed time : ', elapsed)
 
 main()
 
